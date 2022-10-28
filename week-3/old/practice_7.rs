be2fe9181302d8502400d169516ef301e7d449a6
dmoru883 <dmoru@pau.edu.ng>
2022-10-28 17:19:31 +0100

    (My best commit)

--- week-3/old/practice_7.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-   let special_character = '@'; //default
-   let alphabet:char = 'A';
-   
-   
-   println!("special character is {}",special_character);
-   println!("alphabet is {}",alphabet);
-   
-}
--- week-3/old/practice_8/target/debug/practice_8.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Moruson\Documents\d.moruCSC101\week-3\practice_8\target\debug\practice_8.exe: C:\Users\Moruson\Documents\d.moruCSC101\week-3\practice_8\src\main.rs
