40cfb4d81ce89f30c1cea2d5668577ff4cc5b40b
dmoru883 <dmoru@pau.edu.ng>
2022-11-15 10:26:15 +0100

    (Week 4 commit)

---
+++ week-4/practice_3/src/main.rs
@@ -0,0 +1,17 @@
+fn main(){
+   let name1 = "Ayomide Adesokan".to_string(); 
+   println!("My name is {}",name1);
+
+   //find and replace
+   let name2 = name1.replace("Ayomide","Adebare");
+   println!("You can also call me {}",name2);
+   let faculty = "Faculty of Science and Technology";
+
+   //find and replace
+   let school = faculty.replace("Faculty", "School");
+   println!("I am a student of the {}", school);
+
+}
+
+
+
